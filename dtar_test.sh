cedf133217e99be022aae0a52b44101d8f1ff8ba
Evgeny V. Bogodukhov <b@evbg.ru>
2019-03-27 20:05:07 +0400

    Added loading of the test framework shunit2 in case of its absence at the moment of launching the test

--- dtar_test.sh
+++ dtar_test.sh
@@ -10,4 +10,9 @@ test_shellcheck() {
 # Load shUnit2
 # shUnit2 is a xUnit based unit test framework for Bourne based shell scripts.
 # https://github.com/kward/shunit2
+
+if ! test -e "./shunit2"; then
+	  git clone https://github.com/kward/shunit2
+fi
+
 . ./shunit2/shunit2
